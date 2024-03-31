7fe56e190e75d6f1ec2f4838f41f51460394a508
Ruben Schlaen <gschlaen@agro.uba.ar>
2024-03-31 16:03:08 -0300

    Handle Loading, Data and Error states in MoviesListScreen

--- lib/src/features/movies/presentation/movies_list/movies_list_screen.dart
+++ lib/src/features/movies/presentation/movies_list/movies_list_screen.dart
@@ -1,13 +1,46 @@
+import 'package:coolmovies/src/features/movies/data/movies_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MoviesListScreen extends StatelessWidget {
+class MoviesListScreen extends ConsumerWidget {
   const MoviesListScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('MoviesListScreen'),
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cool Movies'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Consumer(
+              builder: (BuildContext context, WidgetRef ref, Widget? child) {
+                final movies = ref.watch(moviesListProvider);
+
+                return movies.when(
+                  data: (data) => ListView.builder(
+                    itemCount: data.length,
+                    itemBuilder: (context, index) {
+                      final movie = data[index];
+                      // TODO: replace with a custom MovieListTile
+                      return ListTile(
+                        leading: Image(image: NetworkImage(movie.imgUrl)),
+                        title: Text(movie.title),
+                        subtitle: Text(movie.releaseDate.toString()),
+                        onTap: () {},
+                      );
+                    },
+                  ),
+                  // TODO: improve error style
+                  error: (e, st) => Center(child: Text(e.toString())),
+                  loading: () =>
+                      const Center(child: CircularProgressIndicator()),
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
