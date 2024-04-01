a9d59cceeb4231a38fa0bcd8e9420a25a2d4b133
Ruben Schlaen <gschlaen@agro.uba.ar>
2024-04-01 14:33:10 -0300

    Show reviews in MovieDetailsScreen

--- lib/src/features/movies/presentation/movie_details/movie_details_screen.dart
+++ lib/src/features/movies/presentation/movie_details/movie_details_screen.dart
@@ -1,5 +1,6 @@
 import 'package:coolmovies/src/features/movies/data/movie_director_repository.dart';
 import 'package:coolmovies/src/features/movies/domain/movie.dart';
+import 'package:coolmovies/src/features/reviews/data/reviews_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -15,22 +16,50 @@ class MovieDetailsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text(movie.title)),
       // TODO: improve UI
-      body: ListView(children: [
-        Image(image: NetworkImage(movie.imgUrl)),
-        Text(movie.releaseDate),
-        Consumer(
-          builder: (BuildContext context, WidgetRef ref, Widget? child) {
-            final movieDirector =
-                ref.watch(movieDirectorProvider(movie.movieDirectorId));
-            return movieDirector.when(
-              data: (data) => Text("${data.name} (${data.age})"),
-              // TODO: improve error style
-              error: (e, st) => Text(e.toString()),
-              loading: () => const CircularProgressIndicator(),
-            );
-          },
-        ),
-      ]),
+      body: ListView(
+        children: [
+          Image(image: NetworkImage(movie.imgUrl)),
+          Text(movie.releaseDate),
+          Consumer(
+            builder: (BuildContext context, WidgetRef ref, Widget? child) {
+              final movieDirector =
+                  ref.watch(movieDirectorProvider(movie.movieDirectorId));
+              return movieDirector.when(
+                data: (data) => Text("${data.name} (${data.age})"),
+                // TODO: improve error style
+                error: (e, st) => Text(e.toString()),
+                loading: () => const Center(child: CircularProgressIndicator()),
+              );
+            },
+          ),
+          Consumer(
+            builder: (BuildContext context, WidgetRef ref, Widget? child) {
+              final reviews = ref.watch(reviewsProvider(movieId));
+              return reviews.when(
+                // TODO: improve UI
+                data: (data) => Column(
+                  children: data
+                      .map((e) => ListTile(
+                            leading: Text(e.rating.toString()),
+                            title: Text(e.title),
+                            subtitle: Text(e.body),
+                            // TODO: Show only in user's review
+                            trailing: GestureDetector(
+                              child: const Icon(Icons.delete),
+                              // TODO: Implement delete review
+                              onTap: () {},
+                            ),
+                          ))
+                      .toList(),
+                ),
+                // TODO: improve error style
+                error: (e, st) => Text(e.toString()),
+                loading: () => const Center(child: CircularProgressIndicator()),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
