36b983b448f334553903a14e5b30e1fb0a434f2e
Ruben Schlaen <gschlaen@agro.uba.ar>
2024-04-02 17:54:07 -0300

    Add leaveReviewController to handle submitReview states

---
+++ lib/src/features/reviews/presentation/leave_review/leave_review_controller.dart
@@ -0,0 +1,45 @@
+import 'dart:async';
+
+import 'package:coolmovies/src/features/reviews/application/reviews_service.dart';
+import 'package:coolmovies/src/features/reviews/data/reviews_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class LeaveReviewController extends AutoDisposeAsyncNotifier<void> {
+  bool mounted = true;
+
+  @override
+  FutureOr<void> build() {
+    ref.onDispose(() => mounted = false);
+  }
+
+  Future<void> submitReview({
+    required String movieId,
+    required String title,
+    required String body,
+    required int rating,
+  }) async {
+    final reviewService = ref.read(reviewServiceProvider);
+    state = const AsyncLoading();
+    final newState = await AsyncValue.guard(() => reviewService.submitReview(
+          movieId: movieId,
+          title: title,
+          body: body,
+          rating: rating,
+        ));
+
+    if (mounted) {
+      // * only set the state if the controller hasn´t been disposed
+      state = newState;
+      if (state is AsyncData) {
+        // * invalidate the reviewsProvider to refetch the reviews
+        ref.invalidate(reviewsProvider(movieId));
+        ref.read(reviewsProvider(movieId));
+      }
+    }
+  }
+}
+
+final leaveReviewControllerProvider =
+    AsyncNotifierProvider.autoDispose<LeaveReviewController, void>(() {
+  return LeaveReviewController();
+});
